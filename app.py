08da2469d214e4d03022bbe78ed4c26aa2b174e4
Zinolesky <taigbenuefezino@gmail.com>
2026-09-19 00:45:05 +0100

    Add DB_PORT for Railway connection

--- app.py
+++ app.py
@@ -14,7 +14,8 @@ def get_db():
         host=os.environ.get("DB_HOST", "localhost"),
         user=os.environ.get("DB_USER", "root"),
         password=os.environ.get("DB_PASSWORD", ""),
-        database=os.environ.get("DB_NAME", "bincomphptest")
+        database=os.environ.get("DB_NAME", "bincomphptest"),
+        port=os.environ.get("DB_PORT", "3306")
     )
 
 
