d80d22da0dca0203d710f75d3b5e2892c30360d0
Zinolesky <taigbenuefezino@gmail.com>
2026-09-19 03:06:07 +0100

    Update default port to 8080

--- app.py
+++ app.py
@@ -136,5 +136,5 @@ def store_result():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port, debug=True)
